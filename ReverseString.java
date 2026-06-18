ea5c42e33366f2cec8a472745ba3e5607da83065
anushka-codes07 <singhanushka1604@gmail.com>
2026-06-18 23:26:56 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+package Basics;
+import java.util.*;
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string to reverse: ");
+        String original = sc.nextLine();
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        System.out.println("Reversed string: " + reversed);
+        sc.close();
+    }
+}
