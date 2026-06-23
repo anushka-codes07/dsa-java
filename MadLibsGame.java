003f599d4a7ecabe829bb0706b6259bd4a7809c0
anushka-codes07 <singhanushka1604@gmail.com>
2026-06-23 17:38:51 +0530

    Add files via upload

---
+++ MadLibsGame.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class MadLibsGame {
+    public static void main(String[] args) {
+        //MAD LIBS GAME
+        Scanner sc = new Scanner(System.in);
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = sc.nextLine();
+        System.out.print("Enter a noun (animal or person): ");
+        noun1 = sc.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = sc.nextLine();
+        System.out.print("Enter a verb end with -ing: ");
+        verb1 = sc.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = sc.nextLine();
+
+        System.out.println("\nToday I went to a " + adjective1 + " Zoo.");
+        System.out.println("In an exhibit, I saw  " + noun1 + " .");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + " !");
+        System.out.println("I was " + adjective3 + " !");
+
+        sc.close();
+    }
+}
